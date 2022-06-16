0437c96f65db3e213d94e0dda3e487c10d54f10a
Francois Chasseur <francois@labo21.be>
2022-06-16 16:59:58 +0200

    MATRIX-4907:Adding a mecanism to rename the project - fine tuning

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -46,7 +46,7 @@ do
    [[ $filename != .github* ]] &&  sed -i "s/$original_pageid/$pageid/g" "$filename"
    [[ $filename != .github* ]] &&  sed -i "s/$original_plugin/$plugin/g" "$filename"
    [[ $filename != .github* ]] &&  echo "$filename fixed"
-   [[ $filename != _*.ts ]] && mv $filename $pageid.$filename
+   [[ $filename = _*.ts ]] && git mv $filename $pageid.$filename
  
 done
 

fe9b8b32c9240073542c808c3c9138e78b187a20
Tilman Schlenker <tilman@mailbox.org>
2023-06-28 22:21:05 +0200

    fix open, cleanup

--- dev-proxy/index.ts
+++ dev-proxy/index.ts
@@ -7,11 +7,12 @@ import * as mockttp from "mockttp"
 (async () => {
     if (process.argv.length < 4) {
         console.error("You need to provide the name of the instance as the second parameter");
-        return;
-        // process.exit(-1);
+        process.exit(-1);
     }
     const instance = process.argv[2]
     const scriptFile = process.argv[3]
+    
+    const scriptname = "mypluginscript.js"
     
     // Create a proxy server with a self-signed HTTPS CA certificate:
     const https = await mockttp.generateCACertificate();
@@ -20,26 +21,34 @@ import * as mockttp from "mockttp"
     const target = `${instance}.matrixreq.com`
     const url = `https://${target}`
 
-    const addedUrl = `<script src="/mypluginscript.js"></script>`
+    const addedUrl = `<script src="/${scriptname}"></script>`
     const replaced = `${addedUrl}\n</body>\n</html>`
     const page = await fetch(url).then(data => data.text())
     const patchedPage = page.replace(/<\/body>\s*<\/html>/, replaced)
 
-    await server.forGet(url).always().thenReply(200, patchedPage)
-    await server.forGet(`${url}/mypluginscript.js`).always().thenFromFile(200, `../${scriptFile}`)
-    await server.forAnyRequest().forHostname(target).always().thenPassThrough()
-    // await server.forAnyRequest().thenFromFile(404, "./default.html")
-    // await server.forAnyRequest().thenPassThrough();
+    await server.forAnyWebSocket().always()
+        .thenPassThrough();
+    await server.forGet(url).always()
+        .thenReply(200, patchedPage)
+    await server.forGet(`${url}/${scriptname}`).always()
+        .thenFromFile(200, `../${scriptFile}`)
+    await server.forAnyRequest().forHostname(target).always()
+        .thenPassThrough()
     await server.start();
 
     const caFingerprint = mockttp.generateSPKIFingerprint(https.cert);
 
     if (process.argv[4] === 'chrome') {
         // Launch an intercepted Chrome using this proxy:
-        launchChrome(url, server, caFingerprint);
+        await launchChrome(url, server, caFingerprint);
+        process.on('SIGINT', function() {
+            console.log("Caught interrupt signal");
+            // Find a way to kill the Chrome process
+            process.exit();
+        });
     } else {
         // Print out the server details for manual configuration:
         console.log(`Server running on port ${server.port}`);
         console.log(`CA cert fingerprint ${caFingerprint}`);
     }
-})(); // (All run in an async wrapper, so we can easily use top-level await)
+})();

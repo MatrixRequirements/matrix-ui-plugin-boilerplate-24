7ba268005acc1e35a182b0e47c555151e549c486
Yuri Nezdemkovski <yuri@nezdemkovski.com>
2024-01-29 17:21:04 +0100

    refactor dashboard to modern ts

--- src/BPP/Dashboard/DashboardPage.tsx
+++ src/BPP/Dashboard/DashboardPage.tsx
@@ -2,45 +2,43 @@ import * as ReactDOM from "react-dom";
 
 import { Plugin } from "../Plugin";
 import { DashboardPageContainer } from "./Components/DashboardPageContainer";
-import { DashboardProps, DashboardState, IDashboard, IDashboardContent, IProjectSettings } from "../Interfaces";
+import { IDashboard, IDashboardContent, IProjectSettings } from "../Interfaces";
 import { Project, IDataStorage, IItem, IDashboardPage, IDashboardParametersBase } from "matrix-requirements-sdk/client";
 import { sdkInstance } from "./../Instance";
 
 export interface IDashboardParameters extends IDashboardParametersBase {}
 
-export const Dashboard = (props: IDashboardContent) => <div className="itemDetails">Hello from Dashboard!</div>;
-
-// Glue code to support the IDashboardPage interface
-// eslint-disable-next-line no-unused-vars
 export class DashboardPage implements IDashboardPage<IDashboardParameters> {
-    settings: IProjectSettings;
+    private settings: IProjectSettings;
 
     constructor(
-        private project: Project,
-        private projectStorage: IDataStorage,
-        private popupModeOrControl = false,
-        // @ts-ignore
-        private currentFolder: IItem = undefined,
+        private readonly project: Project,
+        private readonly projectStorage: IDataStorage,
+        private readonly popupModeOrControl: boolean = false,
+        private readonly currentFolder?: IItem,
     ) {
-        // @ts-ignore
-        this.settings = {
+        this.settings = this.initializeSettings();
+    }
+
+    private initializeSettings(): IProjectSettings {
+        return {
             ...Plugin.config.projectSettingsPage.defaultSettings,
-            ...project.getItemConfig().getSettingJSON(Plugin.config.projectSettingsPage.settingName, {}),
+            ...this.project.getItemConfig().getSettingJSON(Plugin.config.projectSettingsPage.settingName, {}),
         };
     }
 
-    /** Add interactive element in this function */
-    renderProjectPage() {
-        const element = document.createElement("div");
-        let dashboard: IDashboard = {
+    renderProjectPage(): void {
+        const dashboard: IDashboard = {
             header: { title: "Dashboard", showFullScreen: false },
             dashboardContent: { settings: this.settings },
         };
+
+        const element = document.createElement("div");
         ReactDOM.render(<DashboardPageContainer dashboard={dashboard} />, element);
         sdkInstance.app.itemForm.append(element);
     }
 
-    onResize() {
-        /* Will be triggered when resizing. */
+    onResize(): void {
+        // Resize handling logic goes here.
     }
 }
